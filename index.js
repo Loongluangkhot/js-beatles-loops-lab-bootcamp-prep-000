b74b6d0fd5f10233ff3c38c434577808f3d5e315
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-15 13:29:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 function theBeatlesPlay(arrayMusician,arrayInstrument) {
+  // create empty array stored in a var
+  var array = {}
+  // for loop, loops over array of musicians
+  // first loop: create a string using first index of musician array, and first index of instrument, `${arrayMusician[i]} plays ${arrayInstrument[i]}`, add the string to the empty array created
   
 }
